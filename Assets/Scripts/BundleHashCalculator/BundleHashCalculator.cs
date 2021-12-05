d8334295a9ef0624b1f8d2c449e9bce7d28adcc5
Alexey Vashov <v.alex@belka.app>
2021-12-05 17:37:09 +0700

    added BundleHashCalculator

---
+++ Assets/Scripts/BundleHashCalculator/BundleHashCalculator.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using UnityEngine;
+
+namespace Scripts.BundleHashCalculator
+{
+    public enum NameDeveloper
+    {
+        None,
+        Developer1,
+        Developer2,
+        Developer3
+    }
+
+    [CreateAssetMenu(fileName = "BundleHashCalculatorConfig", menuName = "Bundle/Hash calculator")]
+    public sealed class BundleHashCalculator : ScriptableObject
+    {
+        public List<InfoDeveloper> Developers;
+        public NameDeveloper SelectedDeveloperPreset;
+
+        [Header("Название бандла в папке AssetBundle")]
+        public string NameBundle;
+        [Header("Хэши, которые надо вставить в versionBundle")]
+        public string HashAndroid;
+        public string HashIOS;
+        public string HashWindows;
+
+        [ContextMenu("Apply All")]
+        public bool ApplyAll()
+        {
+            try
+            {
+                return CalculateHash() && CopyFileToFolderByPresetDeveloper();
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+        }
+
+        [ContextMenu("Calculate Hash")]
+        public bool CalculateHash()
+        {
+            try
+            {
+                var tempPathFileUnity3d = GetPathFileUnity3d(RuntimePlatform.Android, NameBundle.ToLower());
+                HashAndroid = ComputeMD5Checksum(tempPathFileUnity3d);
+
+                tempPathFileUnity3d = GetPathFileUnity3d(RuntimePlatform.IPhonePlayer, NameBundle.ToLower());
+                HashIOS = ComputeMD5Checksum(tempPathFileUnity3d);
+
+                tempPathFileUnity3d = GetPathFileUnity3d(RuntimePlatform.WindowsEditor, NameBundle.ToLower());
+                HashWindows = ComputeMD5Checksum(tempPathFileUnity3d);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                return false;
+            }
+        }
+
+        public bool CheckPresetDeveloper()
+        {
+            var presetDeveloper = Developers.Find(developer => developer.NameDeveloper == SelectedDeveloperPreset);
+            return presetDeveloper != null;
+        }
+
+        public bool CheckPresetPathToGitFolder()
+        {
+            var presetDeveloper = Developers.Find(developer => developer.NameDeveloper == SelectedDeveloperPreset);
+            return presetDeveloper != null && !string.IsNullOrEmpty(presetDeveloper.PathToGitPublicAssetBundle);
+        }
+
+        [ContextMenu("Copy File")]
+        public bool CopyFileToFolderByPresetDeveloper()
+        {
+            try
+            {
+                var presetDeveloper = Developers.Find(developer => developer.NameDeveloper == SelectedDeveloperPreset);
+
+                if (!CheckPresetDeveloper())
+                {
+                    Debug.LogError("Not find preset developer with name: " + SelectedDeveloperPreset.ToString());
+                    return false;
+                }
+
+                if (!CheckPresetPathToGitFolder())
+                {
+                    Debug.LogError("Not set path to folder with git bundles, please check preset developer with name: " + SelectedDeveloperPreset.ToString());
+                    return false;
+                }
+
+                CopyFilesToFolder(presetDeveloper.PathToGitPublicAssetBundle, RuntimePlatform.Android);
+                CopyFilesToFolder(presetDeveloper.PathToGitPublicAssetBundle, RuntimePlatform.IPhonePlayer);
+                CopyFilesToFolder(presetDeveloper.PathToGitPublicAssetBundle, RuntimePlatform.WindowsEditor);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private void CopyFilesToFolder(string pathToFolder, RuntimePlatform platform)
+        {
+            var tempPathFileUnity3d = GetPathFileUnity3d(RuntimePlatform.Android, NameBundle.ToLower());
+            string destFile = System.IO.Path.Combine(pathToFolder, (GetPathBundle(platform) + NameBundle).ToLower());
+
+            System.IO.Directory.CreateDirectory(pathToFolder);
+            System.IO.File.Copy(tempPathFileUnity3d, destFile, true);
+
+            tempPathFileUnity3d = GetPathFileUnity3d(RuntimePlatform.Android, NameBundle.ToLower() + ".manifest");
+            destFile = System.IO.Path.Combine(pathToFolder, (GetPathBundle(platform) + NameBundle).ToLower() + ".manifest");
+
+            System.IO.Directory.CreateDirectory(pathToFolder);
+            System.IO.File.Copy(tempPathFileUnity3d, destFile, true);
+        }
+
+        private string ComputeMD5Checksum(string path)
+        {
+            try
+            {
+                using (FileStream fs = System.IO.File.OpenRead(path))
+                {
+                    MD5 md5 = new MD5CryptoServiceProvider();
+                    byte[] fileData = new byte[fs.Length];
+                    fs.Read(fileData, 0, (int)fs.Length);
+                    byte[] checkSum = md5.ComputeHash(fileData);
+                    string result = BitConverter.ToString(checkSum).Replace(" - ", String.Empty);
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                return "";
+            }
+        }
+
+        private string GetPathBundle(RuntimePlatform platform)
+        {
+            switch (platform)
+            {
+                case RuntimePlatform.Android:
+                    return "Android\\";
+                case RuntimePlatform.IPhonePlayer:
+                    return "iOS\\";
+                case RuntimePlatform.WindowsPlayer:
+                case RuntimePlatform.LinuxEditor:
+                case RuntimePlatform.OSXEditor:
+                case RuntimePlatform.WindowsEditor:
+                    return "Windows\\";
+                default: throw new Exception($"Domain.name for {Application.platform} is not found");
+            }
+        }
+
+        private string GetPathFileUnity3d(RuntimePlatform pltafrom, string bundleName) =>
+            Path.Combine(@"C:\Users\" + Environment.UserName + @"\Desktop\AssetBundles\", GetPathBundle(pltafrom) + bundleName);
+    }
+
+    [Serializable]
+    public sealed class InfoDeveloper
+    {
+        public NameDeveloper NameDeveloper;
+        public string PathToGitPublicAssetBundle;
+    }
+}
---
+++ Assets/Scripts/BundleHashCalculator/Editor/BundleHashCalculatorEditor.cs
@@ -0,0 +1,144 @@
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Scripts.BundleHashCalculator
+{
+    [CustomEditor(typeof(BundleHashCalculator))]
+    public class BundleHashCalculatorEditor : Editor
+    {
+        private BundleHashCalculator _hashBundleCalculator;
+
+        private SerializedObject _objectSO;
+        private ReorderableList _listInfoDevelopers;
+        private bool _isEnableSettings;
+        private bool _isOperationSuccess = false;
+        private bool _isCallOperation = false;
+
+        private string _cachBundleName;
+        private string _bundleFolder;
+        private NameDeveloper _cachNameDeveloper;
+
+        private void OnEnable()
+        {
+            _hashBundleCalculator = (BundleHashCalculator)target;
+            _isOperationSuccess = false;
+            _isCallOperation = false;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (_cachBundleName != _hashBundleCalculator.NameBundle
+                || _cachNameDeveloper != _hashBundleCalculator.SelectedDeveloperPreset)
+            {
+                _isOperationSuccess = false;
+                _isCallOperation = false;
+            }
+            _cachBundleName = _hashBundleCalculator.NameBundle;
+            _cachNameDeveloper = _hashBundleCalculator.SelectedDeveloperPreset;
+
+            GUILayout.Space(10);
+
+            var styleMainLabel = new GUIStyle(GUI.skin.label);
+            styleMainLabel.normal.textColor = Color.white;
+            styleMainLabel.fontSize = 20;
+            styleMainLabel.alignment = TextAnchor.MiddleCenter;
+            EditorGUILayout.LabelField("Hash Calculator", styleMainLabel);
+
+            GUILayout.Space(10);
+
+            var rect = EditorGUILayout.BeginHorizontal();
+            Handles.color = Color.gray;
+            Handles.DrawLine(new Vector2(rect.x - 15, rect.y), new Vector2(rect.width + 15, rect.y));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+
+            _hashBundleCalculator.SelectedDeveloperPreset = (NameDeveloper)EditorGUILayout.EnumPopup("Пресет разработчика", _hashBundleCalculator.SelectedDeveloperPreset);
+            _hashBundleCalculator.NameBundle = EditorGUILayout.TextField("Имя бандла", _hashBundleCalculator.NameBundle);
+
+            bool isSelectPresetDeveloper = _hashBundleCalculator.SelectedDeveloperPreset != NameDeveloper.None;
+            bool isExistNameBundle = !string.IsNullOrEmpty(_hashBundleCalculator.NameBundle);
+            bool isExistPresetDeveloper = _hashBundleCalculator.CheckPresetDeveloper();
+            bool isExistPathToFolderGit = _hashBundleCalculator.CheckPresetPathToGitFolder();
+
+            var originalColor = GUI.backgroundColor;
+            GUI.backgroundColor = Color.green;
+
+            EditorGUI.BeginDisabledGroup(!(isExistPathToFolderGit && isSelectPresetDeveloper && isExistNameBundle));
+            {
+                if (GUILayout.Button("Apply all", GUILayout.Height(30)))
+                {
+                    _isCallOperation = true;
+                    _isOperationSuccess = _hashBundleCalculator.ApplyAll();
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUI.BeginDisabledGroup(!isExistNameBundle);
+            {
+                GUI.backgroundColor = originalColor;
+                if (GUILayout.Button("Calculate hash", GUILayout.Height(30)))
+                {
+                    _isCallOperation = true;
+                    _isOperationSuccess = _hashBundleCalculator.CalculateHash();
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUI.BeginDisabledGroup(!(isExistPathToFolderGit && isSelectPresetDeveloper && isExistNameBundle));
+            {
+                if (GUILayout.Button("Copy bundles to folder", GUILayout.Height(30)))
+                {
+                    _isCallOperation = true;
+                    _isOperationSuccess = _hashBundleCalculator.CopyFileToFolderByPresetDeveloper();
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+
+            if (_isCallOperation)
+            {
+                var style = new GUIStyle(GUI.skin.label);
+                style.fontSize = 12;
+                style.alignment = TextAnchor.MiddleCenter;
+                if (_isOperationSuccess)
+                {
+                    style.normal.textColor = Color.green;
+                    EditorGUILayout.LabelField("Operation completed successfully!", style);
+                }
+                else
+                {
+                    style.normal.textColor = Color.red;
+                    EditorGUILayout.LabelField("Operation failed!", style);
+                }
+            }
+
+            EditorGUILayout.Space();
+            rect = EditorGUILayout.BeginHorizontal();
+            Handles.color = Color.gray;
+            Handles.DrawLine(new Vector2(rect.x - 15, rect.y), new Vector2(rect.width + 15, rect.y));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Settings", GUILayout.Height(30)))
+            {
+                _isEnableSettings = !_isEnableSettings;
+            }
+
+            if (_isEnableSettings)
+            {
+                var styleLabelSettings = new GUIStyle(GUI.skin.label);
+                styleLabelSettings.normal.textColor = Color.white;
+                styleLabelSettings.fontSize = 14;
+                EditorGUILayout.LabelField("Adding developer presets", styleLabelSettings);
+
+                serializedObject.Update();
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("Developers"), true);
+                serializedObject.ApplyModifiedProperties();
+
+                _hashBundleCalculator.HashAndroid = EditorGUILayout.TextField("Android", _hashBundleCalculator.HashAndroid);
+                _hashBundleCalculator.HashIOS = EditorGUILayout.TextField("IOS", _hashBundleCalculator.HashIOS);
+                _hashBundleCalculator.HashWindows = EditorGUILayout.TextField("Windows", _hashBundleCalculator.HashWindows);
+            }
+        }
+    }
+}
